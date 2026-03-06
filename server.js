af82224667ddec4c891584f7609f4918316254ed
Amar7564 <amarsatishmandalapu20@gmail.com>
2026-03-06 18:26:35 +0530

    Add Health check piont

--- server.js
+++ server.js
@@ -39,3 +39,7 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
+
+app.get("/health", (req, res) => {
+  res.json({ status: "API Working" });
+});
